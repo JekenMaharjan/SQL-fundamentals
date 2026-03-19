7ee38a56f8b20c2d5e52a8bd2de3e5f6862f0c10
JekenMaharjan <maharjanjeken@gmail.com>
2026-03-19 23:18:07 +0545

    01_basics: Sorting & Limiting (ORDER BY, ASC/DESC & LIMIT)

--- 01_basics.sql
+++ 01_basics.sql
@@ -1,7 +1,7 @@
 -- 1. Create database
 CREATE DATABASE practice_db;
 
--- =====================================================================
+-- =========================================================================================
 
 -- 2. Use database
 USE practice_db;
@@ -9,7 +9,7 @@ USE practice_db;
 -- It tells MySQL:
 -- “From now on, run all queries inside this database”
 
--- =====================================================================
+-- =========================================================================================
 
 -- 3. Create users table
 CREATE TABLE users (
@@ -19,7 +19,7 @@ CREATE TABLE users (
     email VARCHAR(100)
 );
 
--- =====================================================================
+-- =========================================================================================
 
 -- 4. Create tasks table
 CREATE TABLE tasks (
@@ -29,7 +29,7 @@ CREATE TABLE tasks (
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
--- =====================================================================
+-- =========================================================================================
 
 -- 5. Insert users
 INSERT INTO users (name, age, email) VALUES
@@ -37,7 +37,7 @@ INSERT INTO users (name, age, email) VALUES
 ('Shyam', 15, 'Shyam@gmail.com'),
 ('Hari', 36, 'Hari@gmail.com');
 
--- =====================================================================
+-- =========================================================================================
 
 -- 6. Insert tasks
 INSERT INTO tasks (title, user_id) VALUES
@@ -45,20 +45,19 @@ INSERT INTO tasks (title, user_id) VALUES
 ('Build Project', '1'),
 ('Practice Coding', '2');
 
--- =====================================================================
+-- =========================================================================================
 
 -- 7. Get all users
-SELECT *
-FROM users;
+SELECT * FROM users;
 
--- =====================================================================
+-- =========================================================================================
 
 -- 8. Get users.name and tasks.title using JOIN
 SELECT users.name, tasks.title
 FROM users
 JOIN tasks ON users.id = tasks.user_id;
 
--- =====================================================================
+-- =========================================================================================
 
 -- 9. Perform CRUD operations on users table
 
@@ -82,7 +81,7 @@ WHERE user_id = 1;
 DELETE FROM users
 WHERE id = 1;
 
--- =====================================================================
+-- =========================================================================================
 
 -- 10. Filtering Data
 
@@ -137,8 +136,56 @@ AND NOT name = 'Ram';
 SELECT * FROM users
 WHERE name IN ('Ram', 'Hari');
 
--- =====================================================================
+-- Get users with id 1 or 3
+SELECT * FROM users
+WHERE id IN (1, 3);
 
+-- Get tasks of specific users (Ram and Shyam)
+SELECT users.name, tasks.title
+FROM users
+JOIN tasks ON users.id = tasks.user_id
+WHERE users.name IN ('Ram', 'Shyam');
 
+-- =========================================================================================
 
+-- 11. Sorting & Limiting
 
+-- ORDER BY
+-- ASC / DESC
+
+-- Sort Users by Name (by default its ASC -> Ascending order)
+SELECT * FROM users
+ORDER BY name
+
+SELECT * FROM users
+ORDER BY name ASC
+
+SELECT * FROM users
+ORDER BY name DESC
+
+-- Sort Tasks by ID (by default its ASC -> Ascending order)
+SELECT * FROM tasks
+ORDER BY id;
+
+SELECT * FROM tasks
+ORDER BY id ASC;
+
+SELECT * FROM tasks
+ORDER BY id DESC;
+
+-- LIMIT
+-- Get First 2 Users
+SELECT * FROM users
+LIMIT 2;
+
+-- Combine ORDER BY + LIMIT
+-- Get the top 1 task (largest id)
+SELECT * FROM tasks
+ORDER BY id DESC
+LIMIT 1;
+
+-- Using LIMIT with Offset (Advanced)
+-- Skip first 1 row, get next 2 rows
+SELECT * FROM users
+ORDER BY id ASC
+LIMIT 1, 2;
